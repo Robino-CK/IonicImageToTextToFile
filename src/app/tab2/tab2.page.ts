c9a3ff3e947b8d4a19a71247922a74358252ff42
Robin-CK <robin.kollmann@yahoo.de>
2021-02-14 19:24:14 +0100

    working so far

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
 import * as Tesseract from 'tesseract.js'
+import * as test from 'C:/Users/robin/OneDrive - rwth-aachen.de/Programms/ionic_kamera_app/photo-gallery/src/app/tab2/test.js'
+
 import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
   CameraPhoto, CameraSource } from '@capacitor/core';
 import { AnyARecord } from 'dns';
 import validator from 'validator';
 var pic;
+//window.require('fs');
+//const ocrad = require('async-ocrad');
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -13,6 +18,7 @@ var pic;
 })
 export class Tab2Page {
   private imageSrc: string = '';
+   recText: string;
   
   constructor(public photoService: PhotoService) {
     
@@ -20,7 +26,12 @@ export class Tab2Page {
  async addPhotoToGallery() { 
     let p =await this.photoService.addNewToGallery();
     this.recognizeImage(p);
-  
+    //let w = await ocrad(p);
+    let f = 2;
+  // this.recText = await test.recognizeImage(p);
+   // let t = 2;
+  //  let w = ocrad.OCRAD(p);
+
     
     
   }
@@ -28,14 +39,18 @@ export class Tab2Page {
   
   recognizeImage(pic: string) {
   
-   
+    this.recText = "loading"
     Tesseract.recognize(pic)
-    
-    .catch(err =>   console.error(err + "2"))
+    .catch(err =>  {
+      console.error(err ) 
+      this.recText = "err"
+    } 
+    )
     .then(result => {
-      var t = result.text;
+      var t = result["data"].text;
       console.log("3")
       localStorage.setItem("text", t);
+      this.recText = t;
     //  this.imageText = result.text;
     })
     .finally(resultOrError => {
