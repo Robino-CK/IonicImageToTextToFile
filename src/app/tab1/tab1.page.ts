c0f1813c9775585c7a19a6b35798343512c0f309
Robin-CK <robin.kollmann@yahoo.de>
2021-02-18 15:03:43 +0100

    sql init

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION  } from "@angular/core";
+import { Component, VERSION } from "@angular/core";
 
 import { DeviceDetectorService } from "ngx-device-detector";
 import { Tab1BothService } from "../UIControllerBoth/tab1-both.service";
@@ -6,6 +6,8 @@ import { Tab1BothService } from "../UIControllerBoth/tab1-both.service";
 
 import { Tap1DesktopService } from "../UIControllerDesktop/tap1-desktop.service";
 import { Tap1MobileService } from "../UIControllerMobile/tap1-mobile.service";
+
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -13,37 +15,78 @@ import { Tap1MobileService } from "../UIControllerMobile/tap1-mobile.service";
 })
 
 
- export  class Tab1Page {
-  public text ;
+export class Tab1Page {
+  public text;
   currentImage: any;
-  
+
 
   constructor(private deviceService: DeviceDetectorService
-   , private tab1ServiceDesktop: Tap1DesktopService, private tab1ServiceMobile: Tap1MobileService, private tab1BothService: Tab1BothService) { }
+    , private tab1ServiceDesktop: Tap1DesktopService, private tab1ServiceMobile: Tap1MobileService, private tab1BothService: Tab1BothService,
+  ) { }
 
-      async takePicture() {
-        
-        if(this.deviceService.isMobile()) {
-          this.tab1ServiceMobile.takePicture().then(text => {
-            this.text = text;
+  async takePicture() {
+
+    if (this.deviceService.isMobile()) {
+      this.tab1ServiceMobile.takePicture().then(text => {
+        this.text = text;
+      });
+
+    } else {
+      this.tab1ServiceDesktop.takePicture().then(text => {
+        this.text = text;
+      });
+    }
+
+  }
+  async takeImage(e: any) {
+    this.text = await this.tab1BothService.takeImage(e)
+
+  }
+  saveData() {
+
+
+
+    var sqlite = new SQLite();
+
+    sqlite.create({
+      name: 'data.db',
+      location: 'default'
+
+    })
+      .then((db: SQLiteObject) => {
+
+
+        db.executeSql('create table danceMoves(name VARCHAR(32))', [])
+          .then(() => {
+
+            console.log('Executed SQL')
+            var sqlite2 = new SQLite();
+
+            sqlite2.create({
+              name: 'data.db',
+              location: 'default'
+
+            }).then((db: SQLiteObject) => {
+              db.executeSql('INSERT INTO danceMoves (name) VALUES ("h")', [])
+              .then(() => {
+                console.log('Executed SQL: Insert')
+              })
+              .catch(e=> {
+                console.log('err on Insert ' +e )
+              })
+            })
+          })
+          .catch(e => {
+            console.log(e)
+
           });
-        
-        } else {
-           this.tab1ServiceDesktop.takePicture().then(text => {
-             this.text = text;
-           });
-        }
 
-      }
-      async takeImage (e:any) {
-          this.text = await this.tab1BothService.takeImage(e)
-          
-        }
-        saveData () {
 
-        }
-      
 
-      
+      })
+      .catch(e => console.log(e));
+
+
+  }
 
 }
